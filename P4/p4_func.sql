6cb514e7eace9759f7f1b55c8b708914117602e5
Christine Tsai <rtg7bs@virginia.edu>
2024-10-27 21:19:44 -0400

    add 3 functions

---
+++ P4/p4_func.sql
@@ -0,0 +1,64 @@
+USE PLAYLIST_PROJECT_CS4750;
+
+-- function 1: get all songs on playlist 
+CREATE FUNCTION GetAllSongsFromPlaylist
+(
+    @playlist_id INT
+)
+RETURNS TABLE
+AS 
+RETURN
+(
+    SELECT s.title, artist_name AS artist, s.duration 
+    FROM playlist_contains pc 
+    INNER JOIN song s ON s.song_id = pc.song_id
+    INNER JOIN artist_performs ap ON s.song_id = ap.song_id
+    INNER JOIN artist a ON a.user_id = ap.user_id
+    WHERE playlist_id = @playlist_id
+);
+GO
+
+-- to call function
+SELECT * FROM GetAllSongsFromPlaylist(1);
+
+-- function 2: get all playlists given a user_id
+CREATE FUNCTION GetAllPlaylistsByUser
+(
+    @user_id INT
+)
+RETURNS TABLE
+AS 
+RETURN
+(
+    SELECT playlist_name, creation_date, num_of_songs, duration
+    FROM playlist p
+    WHERE p.user_id = @user_id
+);
+GO
+
+-- to call function
+SELECT * FROM GetAllPlaylistsByUser(5);
+
+-- function 3: get user's most popular artist based on all of user's playlists
+CREATE FUNCTION GetMostPopularArtistForUser
+(
+    @user_id INT
+)
+RETURNS TABLE
+AS 
+RETURN
+(
+    SELECT TOP 1 artist_name AS top_artist, COUNT(*) AS num_songs
+    FROM playlist p
+    INNER JOIN playlist_contains pc ON p.playlist_id = pc.playlist_id
+    INNER JOIN artist_performs ap ON pc.song_id = ap.song_id
+    INNER JOIN artist a ON a.user_id = ap.user_id
+    WHERE p.user_id = @user_id
+    GROUP BY a.artist_name
+    ORDER BY num_songs DESC 
+
+);
+GO
+
+-- to call function
+SELECT * FROM GetMostPopularArtistForUser(1);
