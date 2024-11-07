59aa7c717af047665effdacf38aa1e3bb9a63fcd
kvc6dv <112603724+kvc6dv@users.noreply.github.com>
2024-11-07 15:21:18 -0500

    update trigger (more error validation)

--- P4/p4_trigger.sql
+++ P4/p4_trigger.sql
@@ -1,3 +1,5 @@
+USE PLAYLIST_PROJECT_CS4750
+GO
 
 CREATE TABLE PlaylistErrors (
     ErrorID INT IDENTITY(1,1) PRIMARY KEY,
@@ -5,9 +7,8 @@ CREATE TABLE PlaylistErrors (
     song_id INT,
     ErrorMessage VARCHAR(255),
     ErrorDate DATETIME DEFAULT GETDATE()
-    );
+);
 GO
-
 
 CREATE TRIGGER trg_InsteadOfInsertPlaylistContains
 ON playlist_contains
@@ -20,19 +21,45 @@ BEGIN
     SELECT @song_id = song_id, @playlist_id = playlist_id
     FROM inserted; 
 
-    -- check if the playlist exists
+    -- check if the playlist exists in the db
     IF NOT EXISTS (
-        SELECT 1 FROM playlist WHERE playlist_id = @playlist_id
+        SELECT 1 
+        FROM playlist 
+        WHERE playlist_id = @playlist_id
     )
     BEGIN
         -- log the error into the PlaylistErrors table
         INSERT INTO PlaylistErrors (playlist_id, song_id, ErrorMessage, ErrorDate)
         VALUES (@playlist_id, @song_id, 'Invalid PlaylistID: Playlist does not exist.', GETDATE());
     END
-        
+
+    -- check if the song exists in the db
+    ELSE IF NOT EXISTS (
+        SELECT 1 
+        FROM song 
+        WHERE song_id = @song_id
+    ) 
+    BEGIN
+        -- log the error into the PlaylistErrors table
+        INSERT INTO PlaylistErrors (playlist_id, song_id, ErrorMessage, ErrorDate)
+        VALUES (@playlist_id, @song_id, 'Invalid SongID: Song does not exist.', GETDATE());    
+    END
+
+    -- check if song is already in the playlist
+    ELSE IF EXISTS (
+        SELECT 1 
+        FROM playlist_contains 
+        WHERE playlist_id = @playlist_id AND song_id = @song_id
+    )
+    BEGIN
+        -- log the error into the PlaylistErrors table
+        INSERT INTO PlaylistErrors (playlist_id, song_id, ErrorMessage, ErrorDate)
+        VALUES (@playlist_id, @song_id, 'Duplicate SongID: Song already exists in the playlist', GETDATE());    
+    END
+ 
     ELSE
     BEGIN
-        -- if the playlist exists, insert the song into the playlist in the playlist_contains table
+        -- insert the song into the playlist in the playlist_contains table
         INSERT INTO playlist_contains (playlist_id, song_id)
         VALUES (@playlist_id, @song_id);
     END
