18a207b6a9413d97542c5c3c085c68d435a6978b
jmc7rn <112505978+jmc7rn@users.noreply.github.com>
2024-11-25 15:37:44 -0500

    Implement get user by id

--- P5/src/main/java/com/cs4750/p5/service/UserService.java
+++ P5/src/main/java/com/cs4750/p5/service/UserService.java
@@ -8,7 +8,7 @@ import com.cs4750.p5.entity.User;
 public interface UserService {
     public abstract ResponseEntity<User> createUser(User user);
     public abstract ResponseEntity<List<User>> getAllUsers();
-    public abstract ResponseEntity<User> getUser(Integer id);
-    public abstract ResponseEntity<User> updateUser(Integer id, User user);
-    public abstract ResponseEntity<User> deleteUser(Integer id);
+    public abstract ResponseEntity<User> getUser(Integer userId);
+    public abstract ResponseEntity<User> updateUser(Integer userId, User user);
+    public abstract ResponseEntity<User> deleteUser(Integer userId);
 }
--- P5/src/main/java/com/cs4750/p5/service/UserServiceImpl.java
+++ P5/src/main/java/com/cs4750/p5/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cs4750.p5.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,15 +25,21 @@ public class UserServiceImpl implements UserService {
         return new ResponseEntity<>(userList, HttpStatus.OK);
     }
     
-    public ResponseEntity<User> getUser(Integer id) {
+    public ResponseEntity<User> getUser(Integer userId) {
+        Optional<User> userData = repository.findById(userId);
+        if (userData.isPresent()) {
+            return new ResponseEntity<>(userData.get(), HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public ResponseEntity<User> updateUser(Integer userId, User user) {
         return null;
     }
 
-    public ResponseEntity<User> updateUser(Integer id, User user) {
-        return null;
-    }
-
-    public ResponseEntity<User> deleteUser(Integer id) {
+    public ResponseEntity<User> deleteUser(Integer userId) {
         return null;
     }
 }
