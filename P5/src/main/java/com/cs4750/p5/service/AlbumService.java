b12670524fb392e4599b879b0fb3484ffe63cf0e
kvc6dv <112603724+kvc6dv@users.noreply.github.com>
2024-11-26 16:12:49 -0500

    Update AlbumServices and pom.xml

--- P5/src/main/java/com/cs4750/p5/service/AlbumService.java
+++ P5/src/main/java/com/cs4750/p5/service/AlbumService.java
@@ -1,5 +1,12 @@
 package com.cs4750.p5.service;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import com.cs4750.p5.entity.Album;
 
-public class AlbumService {
-    
+public interface AlbumService {
+    public abstract ResponseEntity<Album> createAlbum(Album album);
+    public abstract ResponseEntity<List<Album>> getAllAlbums();
+    public abstract ResponseEntity<Album> getAlbum(Integer albumId);
+    public abstract ResponseEntity<Album> updateArtist(Integer albumId, Album album);
+    public abstract ResponseEntity<Album> deleteArtist(Integer albumId);
 }
