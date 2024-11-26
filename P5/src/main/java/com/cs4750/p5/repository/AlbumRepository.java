58d24aa5be500273f261a05d605ef962b96cb559
kvc6dv <112603724+kvc6dv@users.noreply.github.com>
2024-11-26 16:05:43 -0500

    Update AlbumRepository

--- P5/src/main/java/com/cs4750/p5/repository/AlbumRepository.java
+++ P5/src/main/java/com/cs4750/p5/repository/AlbumRepository.java
@@ -1,5 +1,9 @@
 package com.cs4750.p5.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.cs4750.p5.entity.Album;
 
-public class AlbumRepository {
-    
+@Repository
+public interface AlbumRepository extends JpaRepository<Album, Integer> {
+
 }
