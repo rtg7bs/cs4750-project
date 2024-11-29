eb7a48b6c351dde5d0587d32ee868d082ada933b
kvc6dv <112603724+kvc6dv@users.noreply.github.com>
2024-11-29 15:00:49 -0500

    Remove import in SongRepository

--- P5/src/main/java/com/cs4750/p5/repository/SongRepository.java
+++ P5/src/main/java/com/cs4750/p5/repository/SongRepository.java
@@ -5,7 +5,6 @@ import com.cs4750.p5.entity.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 
 @Repository
